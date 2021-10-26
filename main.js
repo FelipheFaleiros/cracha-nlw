3271de139bc382c27050b3a440a7b10a76973828
Feliphe Faleiros <69943241+FelipheFaleiros@users.noreply.github.com>
2021-10-26 15:06:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+const LinksSocialMidia = {
+    //objeto
+    github: 'Nicoly',
+    youtube: 'Nicoly',
+    facebook: 'Nicoly',
+    instagram: 'jakeliny.gracielly',
+    twitter: 'Nicoly',
+    apiGit: "FelipheFaleiros",
+};
+  /*const github = "FelipheFaleiros"
+  const youtube = "FelipheFaleiros"
+  const facebook = "FelipheFaleiros"
+  const instagram = "FelipheFaleiros"
+  const twitter = "FelipheFaleiros"*/
+
+  /**
+   * camelCase
+   * PascalCase
+   * snake_case
+   */
+
+  function changeSocialMediaLinks() {
+    //document.getElementById("userName").textContent = "Olivia" //necessário ser feito dessa forma quando NÃO tem o id
+    //userName.textContent = "Maria" //com id
+
+    //lastro de repetição
+    for (let li of socialLinks.children) { //id do ul e referenciando ao filho(as classe que foram setadas)
+        const social =li.getAttribute("class");
+      
+      li.children[0].href=`https://${social}.com/${LinksSocialMidia[social]}`
+
+    //   alert(li.children[0])
+    }
+  }
+
+  changeSocialMediaLinks();
+
+  function getGitHubProfileInfos(){
+      const url = `https://api.github.com/users/${LinksSocialMidia.apiGit}`
+
+      fetch(url)
+      .then(response => response.json())
+    //   .then(data => alert(data.company))
+      .then( data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio 
+        userLink.href = data.html_url
+        userPhoto.src = data.avatar_url
+        userLogin.textContent = data.login
+        
+      })
+      //fetch -> pegara oque for respondido na url, nesse caso será um arq .json
+      //then -> promessa de pegar algo 
+  }    
+  getGitHubProfileInfos()
